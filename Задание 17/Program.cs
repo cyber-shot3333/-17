28344491a6ed59bca2eb5998d79455c9d0ceb8b9
Илья Смирнов <cyber-shot3333@yandex.ru>
2022-01-10 13:30:08 +0300

    Добавьте файлы проекта.

---
+++ Задание 17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_17
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.Write("Введите номер аккаунта:");
+                int accnum1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите номер аккаунта прописью:");
+                string accnum2 = Console.ReadLine();
+                Console.Write("Введите баланс:");
+                double balance1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите имя:");
+                string fname1 = Console.ReadLine();
+                Account<int> account1 = new Account<int>(accnum1, balance1, fname1);
+                Account<string> account2 = new Account<string>(accnum2, balance1, fname1);
+                account1.Print();
+                Console.WriteLine();
+                account2.Print();
+                Console.ReadKey();
+            }
+        }
+        class Account<T>
+        {
+            private T AccNumb;
+            private double Balance;
+            private string FullName;
+            public Account(T accnumb, double balance, string fullname)
+            {
+                AccNumb = accnumb;
+                Balance = balance;
+                FullName = fullname;
+            }
+            public void Print()
+            {
+                Console.Write("Номер аккаунта:{0} ", AccNumb);
+                Console.Write("Баланс:{0} ", Balance);
+                Console.Write("Имя:{0} ", FullName);
+            }
+        }
+    }
